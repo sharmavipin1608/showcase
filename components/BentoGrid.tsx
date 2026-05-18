dfa8c7ecfd96d8faf291ebb9d0a8072369e02b20
Vipin Sharma <sharma.vipin1608@gmail.com>
2026-05-18 08:35:51 -0700

    feat: add BentoGrid layout component

---
+++ components/BentoGrid.tsx
@@ -0,0 +1,17 @@
+import { ProjectTile } from './ProjectTile'
+import type { ProjectData } from '@/lib/types'
+
+export function BentoGrid({ projects }: { projects: ProjectData[] }) {
+  return (
+    <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {projects.map((project) => (
+        <div
+          key={`${project.owner}/${project.repo}`}
+          className={project.featured ? 'sm:col-span-2' : ''}
+        >
+          <ProjectTile project={project} />
+        </div>
+      ))}
+    </div>
+  )
+}
